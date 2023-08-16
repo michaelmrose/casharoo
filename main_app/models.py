2c1b3f8ed9d1f886ee4262245098e3fd36fd3a5a
Rjaysesh0207 <rnishimoto96@gmail.com>
2023-08-16 11:37:46 -0700

    changed model to include decimal field

--- main_app/models.py
+++ main_app/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Account(models.Model):
     name = models.CharField(max_length=255)
-    balance = models.IntegerField()
+    balance = models.DecimalField(max_digits=19, decimal_places=2)
 
+    
 class Transaction(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateTimeField()
-    amount = models.IntegerField()
+    amount = models.DecimalField(max_digits=19, decimal_places=2)
     category = models.CharField(max_length=100)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
